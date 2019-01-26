715496e6554498faefd2f024644819e75fd7c63e
Julian <hello@juliantrueflynn.com>
2019-01-25 19:44:23 -0500

    Init

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+/* eslint react/prefer-stateless-function: 0 */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { Layout, Card } from '@shopify/polaris';
+import '@shopify/polaris/styles.css';
+
+class Index extends React.Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      email: PropTypes.string.isRequired,
+    }),
+  };
+
+  static defaultProps = {
+    user: null,
+  };
+
+  render() {
+    return (
+      <div style={{ padding: '10px 45px' }}>
+        <Head>
+          <title>Dashboard</title>
+          <meta name="description" content="description for indexing bots" />
+        </Head>
+        <Layout>
+          <Card>
+            <p> Dashboard </p>
+            <p>
+              Polaris sample working!
+            </p>
+          </Card>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default Index;

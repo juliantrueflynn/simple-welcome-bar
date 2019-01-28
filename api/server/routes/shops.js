e8c0bddfb20b29cbb062c9ec5c02ac9f87d5e4eb
Julian <hello@juliantrueflynn.com>
2019-01-27 21:31:07 -0500

    Initial tests working for api

--- api/server/routes/shops.js
+++ api/server/routes/shops.js
@@ -2,16 +2,21 @@ const Router = require('koa-router');
 const queries = require('../db/queries/shops');
 
 const router = new Router();
-const BASE_URL = '/api/shops';
+const BASE_URL = '/api/v1/shops';
 
 router.get(BASE_URL, async (ctx) => {
   try {
     const shops = await queries.getAllShops();
 
-    ctx.body = { status: 200, data: shops };
+    console.log('SHOPS', shops)
+
+    ctx.body = {
+      status: 'success',
+      data: shops,
+    };
   } catch (err) {
-    console.log(err); // eslint-disable-line no-console
+    console.log(err)
   }
-});
+})
 
 module.exports = router;

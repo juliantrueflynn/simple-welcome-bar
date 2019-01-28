212b5d4a473cd4aa410eb4bd6273efa26bac9236
Julian <hello@juliantrueflynn.com>
2019-01-27 19:27:27 -0500

    Move api to separate directory

---
+++ api/server/app.js
@@ -0,0 +1,42 @@
+const Koa = require('koa');
+const Next = require('next');
+const bodyParser = require('koa-bodyparser');
+const session = require('koa-session');
+const Router = require('koa-router');
+const shops = require('./controllers/shops');
+
+require('dotenv').config();
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 8000;
+
+const app = Next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
+
+  router.get('*', async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+  });
+
+  router.use(session(server));
+  router.use(bodyParser);
+  server.use(shops.routes());
+  server.use(router.routes());
+
+  server.listen(port, (err) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log(`> Ready on ${process.env.API_URL}`); // eslint-disable-line no-console
+  });
+});
